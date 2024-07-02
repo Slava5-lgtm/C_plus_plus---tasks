c9ba53c4284de80166c198ec4f0616d9dab9be24
Вячеслав <vaceslavsergunin@gmail.com>
2024-07-02 20:50:56 +0500

    +

---
+++ 3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "================================= \n";
+    std::cout << " \n";
+    std::cout << "Hello, \n";
+    std::cout << "Word\n";
+    std::cout << " \n";
+    std::cout << "================================= \n";
+
+
+    std::cout << "=====================================\n";
+    std::cout << "||            Hello,               ||\n";
+    std::cout << "||            Word                 ||\n";
+    std::cout << "=====================================\n";
+
+
+    std::cout << "=====================================\n";
+    std::cout << "||                                 ||\n";
+    std::cout << "||                                 ||\n";
+    std::cout << "||            Hello,               ||\n";
+    std::cout << "||            Word                 ||\n";
+    std::cout << "||                                 ||\n";
+    std::cout << "||                                 ||\n";
+    std::cout << "=====================================\n";
+
+}
